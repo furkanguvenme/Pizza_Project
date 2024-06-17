ee326d447460cb6f43e3b1cc9003ef1f1375d895
furkanguvenme <furkanguven.me@gmail.com>
2024-06-17 18:44:43 +0300

    menu-added

--- src/menu.js
+++ src/menu.js
@@ -5,6 +5,8 @@ export const menu = [
       {
         name: "Shoyu Ramen",
         price: 12,
+        picture:
+          "https://bigoven-res.cloudinary.com/image/upload/t_recipe-256/easy-miso-ramen-soup-1496868.jpg",
         ingredients: [
           { ingredient: "Soy Sauce", cost: 1 },
           { ingredient: "Noodles", cost: 2 },
@@ -14,6 +16,7 @@ export const menu = [
       {
         name: "Miso Ramen",
         price: 14,
+        picture: "https://a.ppy.sh/12197887_1571644376.jpeg",
         ingredients: [
           { ingredient: "Miso Paste", cost: 1.5 },
           { ingredient: "Noodles", cost: 2 },
@@ -23,6 +26,8 @@ export const menu = [
       {
         name: "Tonkotsu Ramen",
         price: 15,
+        picture:
+          "https://i.pinimg.com/736x/72/69/61/72696170c70b47cb67a58876d000ae7b.jpg",
         ingredients: [
           { ingredient: "Pork Bone Broth", cost: 2 },
           { ingredient: "Noodles", cost: 2 },
@@ -37,6 +42,7 @@ export const menu = [
       {
         name: "Margherita",
         price: 10,
+        picture: "https://cs8.pikabu.ru/avatars/2607/x2607423-868906487.png",
         ingredients: [
           { ingredient: "Tomato Sauce", cost: 1 },
           { ingredient: "Mozzarella", cost: 2 },
@@ -53,6 +59,7 @@ export const menu = [
       {
         name: "Pepperoni",
         price: 12,
+        picture: "https://cs8.pikabu.ru/avatars/2607/x2607423-868906487.png",
         ingredients: [
           { ingredient: "Tomato Sauce", cost: 1 },
           { ingredient: "Mozzarella", cost: 2 },
@@ -69,6 +76,8 @@ export const menu = [
       {
         name: "BBQ Chicken",
         price: 14,
+        picture:
+          "https://bigoven-res.cloudinary.com/image/upload/t_recipe-256/bbqchickenpizzas-683f3f.jpg",
         ingredients: [
           { ingredient: "BBQ Sauce", cost: 1.5 },
           { ingredient: "Chicken", cost: 3 },
@@ -90,6 +99,8 @@ export const menu = [
       {
         name: "Cheeseburger",
         price: 8,
+        picture:
+          "https://bigoven-res.cloudinary.com/image/upload/t_recipe-256/wendys-junior-bacon-cheeseburger-copycat-recipe-2301199.jpg",
         ingredients: [
           { ingredient: "Beef Patty", cost: 3 },
           { ingredient: "Cheese", cost: 1 },
@@ -106,6 +117,8 @@ export const menu = [
       {
         name: "Bacon Burger",
         price: 10,
+        picture:
+          "https://media.forgecdn.net/avatars/thumbnails/571/806/256/256/637932355812234040.jpeg",
         ingredients: [
           { ingredient: "Beef Patty", cost: 3 },
           { ingredient: "Bacon", cost: 2 },
@@ -122,6 +135,8 @@ export const menu = [
       {
         name: "Veggie Burger",
         price: 7,
+        picture:
+          "https://bigoven-res.cloudinary.com/image/upload/t_recipe-256/monsterveggieburgers-52497a.jpg",
         ingredients: [
           { ingredient: "Veggie Patty", cost: 2.5 },
           { ingredient: "Lettuce", cost: 0.5 },
@@ -143,6 +158,8 @@ export const menu = [
       {
         name: "Regular Fries",
         price: 3,
+        picture:
+          "https://avatars.mds.yandex.net/i?id=94225f80d1c437ef96cfce9c233ccc3a-5234060-images-thumbs&n=13",
         ingredients: [
           { ingredient: "Potatoes", cost: 1 },
           { ingredient: "Salt", cost: 0.1 },
@@ -152,6 +169,8 @@ export const menu = [
       {
         name: "Cheese Fries",
         price: 4,
+        picture:
+          "https://i.pinimg.com/736x/9d/dc/a8/9ddca8c773fe925f68e7e4408dada4ec.jpg",
         ingredients: [
           { ingredient: "Potatoes", cost: 1 },
           { ingredient: "Cheese Sauce", cost: 1 },
@@ -161,6 +180,8 @@ export const menu = [
       {
         name: "Chili Fries",
         price: 5,
+        picture:
+          "https://yandex-images.clstorage.net/5AaPN2154/5e1bb70bzD/l0Hc8LCN3deRIMg011dsCwnhnR9yR6ZDLXVm00_agEYekkJJ2THbuwbmPMiZCIw3BE92Hhry_ggyql3z48yjm9YUmryrk5LdkCDLOd5MZQwC6ZIXdlOhQXUiBLs8qvYpZ6M-P6JLx7wJ4TuF1g7UaEwNPXIht4ZYTp8SiKSpws6SSP78lrSl4IMGgzrETUArJ7e_IFIami0nRD-hOZrhMGGGERGOUeaeK9EKsYoV7kA0ISRZJN6SXm2FJJmSu_vjl3LA3LKRgf2RPoQm_HJrASWmvg5wO4kuK0Qfiy2AwBJCnQ4Mu0jNsAnzNLO_AMFBIRQYTBj1hHZRgBCouZ3pwaBZxdqvqrfTnTuSBOJYWyI-uYkPYyyaLAxGFLERqNMhbYAdO6FP75EzwjyOjQvudDQQKU8qzYBPUokYj7OH-_C4SMnbiZC-4I82qC7RSH4eKIqlPEwhqQQSax22PILGN0mfMSizT_OuPtU7qqEYw3cEMAhpMMSxTUK7CoynhsvVinjd-5SQo9WABrQjzHF4Lzu9kS5yB4A_DHcfqhyz2Q5vsC0Vo2nclx7COrWqL9lVEh8ceDf4rlJorgWPvafx06hx7euNkqrYugelNP5NZhoyorc5ZCuoHxBpEqUNnfUma70cHZF_7aw3ww6TmiHIVA8jO2854J5TVrUZtoGs6seUVeT8tq6w95cUtgzfcEUKFaKxGFAMiTYIQDyXEYbVFG-1JTWSUfucKfUmmpshzU4nFwJnO_-VanWGBruciOvYjmnt4ZKNneqvHIsj4GtBKzaMtw5lK7kKPV4eoDGv3DxRtxUXpG_GrC7UFIWvJ-NvNj4MYDzNpldBjSWcpo7-77RS49uPlILymByALMdQRz4XhqMNSAalGQNyKJMEpeUCR50xH6JQ8posyDOOkzvDShoKCFw38JdfQpU4i7iL3NCGSMH0jpSAy7chozrxf2U4F4KyLHUYmzkPYTKBO6E",
         ingredients: [
           { ingredient: "Potatoes", cost: 1 },
           { ingredient: "Chili", cost: 2 },
@@ -175,6 +196,8 @@ export const menu = [
       {
         name: "Chicken Nuggets",
         price: 6,
+        picture:
+          "https://avatars.mds.yandex.net/i?id=c3c3a14877114f06452906a19380c38e326287be-4120950-images-thumbs&n=13",
         ingredients: [
           { ingredient: "Chicken", cost: 3 },
           { ingredient: "Breading", cost: 1 },
@@ -191,6 +214,8 @@ export const menu = [
       {
         name: "Hot Dog",
         price: 5,
+        picture:
+          "http://data.shef.tj/api/ProductsImg?img=img_50165_2891_pic.jpg",
         ingredients: [
           { ingredient: "Sausage", cost: 2 },
           { ingredient: "Bun", cost: 1 },
@@ -207,6 +232,8 @@ export const menu = [
       {
         name: "Onion Rings",
         price: 4,
+        picture:
+          "https://4.bp.blogspot.com/-4vDlOk1BHa4/WPg6vYpUlKI/AAAAAAAABPk/QQm8OiXJ9dQVbYmpUaYxHw0nooh8tl_HACLcB/w256-h256-p-k-no-nu/Resep%2BMasakan%2BOnion%2BRing%2Bmudah.jpg",
         ingredients: [
           { ingredient: "Onions", cost: 1 },
           { ingredient: "Breading", cost: 1 },
@@ -228,6 +255,7 @@ export const menu = [
       {
         name: "Cola",
         price: 2,
+        picture: "https://www.one-liners.nl/wp-content/uploads/2014/07/62.png",
         ingredients: [
           { ingredient: "Carbonated Water", cost: 0.5 },
           { ingredient: "Sugar", cost: 0.5 },
@@ -237,6 +265,8 @@ export const menu = [
       {
         name: "Lemonade",
         price: 2.5,
+        picture:
+          "https://static.insales-cdn.com/r/cquqDPRtRvQ/rs:fill-down:256:0:1/q:100/plain/images/collections/1/6171/93419547/large_%D1%81%D0%B8%D1%80%D0%BE%D0%BF1_%D1%81%D0%B6%D0%B0%D1%82.jpg",
         ingredients: [
           { ingredient: "Lemon Juice", cost: 1 },
           { ingredient: "Sugar", cost: 0.5 },
@@ -246,6 +276,8 @@ export const menu = [
       {
         name: "Iced Tea",
         price: 3,
+        picture:
+          "https://bevvyco.s3.amazonaws.com/img/drinks/va/aqva/9979f4e924f3712f64ac8388e8452f21-sm.jpg",
         ingredients: [
           { ingredient: "Tea", cost: 1 },
           { ingredient: "Sugar", cost: 0.5 },
